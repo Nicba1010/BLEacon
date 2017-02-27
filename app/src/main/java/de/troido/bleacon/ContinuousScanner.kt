a2e816151b1e9a5191b78f6e3b0c6a19abf58e0e
Ivan Tokić <toxxmeister@gmail.com>
2017-02-27 16:28:54 +0100

    Scanner update.

--- app/src/main/java/de/troido/bleacon/ContinuousScanner.kt
+++ app/src/main/java/de/troido/bleacon/ContinuousScanner.kt
@@ -3,24 +3,25 @@ package de.troido.bleacon
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.le.*
 import android.os.Handler
+import java.util.*
 
 private const val MAN_ID = 89
 
-private val MAN_UUID_MASK = byteArrayOf(-1, 0, -1, -1, -1, -1)
+private val MAN_UUID32_MASK = byteArrayOf(0, 0, -1, -1, -1, -1)
+private val MAN_UUID_MASK = byteArrayOf(0, 0, -1, -1, -1, -1, -1, -1, -1, -1,
+                                        -1, -1, -1, -1, -1, -1, -1, -1)
 
 private val SCAN_SETTINGS =
         ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_POWER).build()
 
-abstract class ContinuousScanner(val filters: List<ScanFilter>, manufacturerId: Int) {
+abstract class ContinuousScanner(private val filters: List<ScanFilter>) {
     private val handler = Handler()
     private val scanner = obtainScanner()
 
     private val callback = object : ScanCallback() {
         override fun onScanResult(callbackType: Int, result: ScanResult?) {
             super.onScanResult(callbackType, result)
-            result?.scanRecord?.getManufacturerSpecificData(manufacturerId)?.let {
-                onDeviceFound(it.copyOf())
-            }
+            result?.scanRecord?.bytes?.let { onDeviceFound(it.copyOf()) }
         }
     }
 
@@ -36,21 +37,32 @@ abstract class ContinuousScanner(val filters: List<ScanFilter>, manufacturerId: 
     }
 }
 
+fun UUID.toBytes(): ByteArray = hexStringToByteArray(toString().replace("-", ""))
+
+class UuidContinuousScanner(
+        uuid: UUID,
+        private val onFound: (UuidContinuousScanner, Map<Byte, ByteArray>) -> Unit
+) : ContinuousScanner(listOf(ScanFilter.Builder()
+                                     .setManufacturerData(
+                                             MAN_ID,
+                                             byteArrayOf(0, 0) + uuid.toBytes(),
+                                             MAN_UUID_MASK
+                                     )
+                                     .build())) {
+    override fun onDeviceFound(data: ByteArray) = onFound(this, mapOf()) //TODO parsing
+}
+
 class Uuid32ContinuousScanner(
         uuid32: Uuid32,
-        private val onFound: (Uuid32ContinuousScanner, Byte, ByteArray) -> Unit
-) : ContinuousScanner(
-        listOf(ScanFilter.Builder()
-                       .setManufacturerData(
-                               MAN_ID,
-                               byteArrayOf(2, 0) + uuid32.bytes,
-                               MAN_UUID_MASK
-                       )
-                       .build()),
-        MAN_ID
-) {
-    override fun onDeviceFound(data: ByteArray) =
-            onFound(this, data[0], data.copyOfRange(1, data.size))
+        private val onFound: (Uuid32ContinuousScanner, Map<Byte, ByteArray>) -> Unit
+) : ContinuousScanner(listOf(ScanFilter.Builder()
+                                     .setManufacturerData(
+                                             MAN_ID,
+                                             byteArrayOf(0, 0) + uuid32.bytes,
+                                             MAN_UUID32_MASK
+                                     )
+                                     .build())) {
+    override fun onDeviceFound(data: ByteArray) = onFound(this, mapOf()) //TODO parsing
 }
 
 private fun obtainScanner(): BluetoothLeScanner {
