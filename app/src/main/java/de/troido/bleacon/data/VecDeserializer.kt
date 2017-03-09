dca4851555ecb888997a0845c2990712622f4734
Ivan Tokić <toxxmeister@gmail.com>
2017-03-09 19:40:12 +0100

    VecDeserializer index bug fix.

--- app/src/main/java/de/troido/bleacon/data/VecDeserializer.kt
+++ app/src/main/java/de/troido/bleacon/data/VecDeserializer.kt
@@ -3,13 +3,18 @@ package de.troido.bleacon.data
 import de.troido.bleacon.util.joinNull
 
 class VecDeserializer<out X : Primitive>(
-        size: Int,
+        private val size: Int,
         private val deserializer: BleDeserializer<X>
 ) : BleDeserializer<List<X>> {
 
     override val length = deserializer.length * size
 
-    override fun deserialize(data: ByteArray): List<X>? = (0 until length)
-            .map { deserializer.deserialize(data.copyOfRange(it, it + deserializer.length)) }
+    override fun deserialize(data: ByteArray): List<X>? = (0 until size)
+            .map {
+                deserializer.deserialize(data.copyOfRange(
+                        it * deserializer.length,
+                        (it + 1) * deserializer.length
+                ))
+            }
             .joinNull()
 }
