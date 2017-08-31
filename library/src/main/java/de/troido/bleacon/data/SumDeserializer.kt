6e062f09a6b7bc3ca830866902ff27528baab4e8
Ivan Tokić <toxxmeister@gmail.com>
2017-08-31 11:22:56 +0200

    Non-abstract SumDeserializer

--- library/src/main/java/de/troido/bleacon/data/SumDeserializer.kt
+++ library/src/main/java/de/troido/bleacon/data/SumDeserializer.kt
@@ -1,10 +1,9 @@
 package de.troido.bleacon.data
 
-abstract class SumDeserializer<out T> : BleDeserializer<T> {
+class SumDeserializer<out T>(private val deserializers: Map<Byte, BleDeserializer<T>>)
+    : BleDeserializer<T> {
 
     override val length: Int = BleDeserializer.ALL
-
-    abstract val deserializers: Map<Byte, BleDeserializer<T>>
 
     override fun deserialize(data: ByteArray): T? {
         if (data.isEmpty()) return null
