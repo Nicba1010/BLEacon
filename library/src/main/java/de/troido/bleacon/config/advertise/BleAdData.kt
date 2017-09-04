8cd72861e9f13927fc0c9e4de223accd979cbacd
Ivan Tokić <toxxmeister@gmail.com>
2017-09-04 16:39:09 +0200

    BleAdData reorganization

--- library/src/main/java/de/troido/bleacon/config/advertise/BleAdData.kt
+++ library/src/main/java/de/troido/bleacon/config/advertise/BleAdData.kt
@@ -1,7 +1,6 @@
 package de.troido.bleacon.config.advertise
 
 import android.bluetooth.le.AdvertiseData
-import android.os.ParcelUuid
 import de.troido.bleacon.ble.NORDIC_ID
 import de.troido.ekstend.uuid.Uuid16
 import de.troido.ekstend.uuid.bytes
@@ -25,28 +24,3 @@ fun bleAdData(uuid16: Uuid16? = null,
                     }.apply(build)
                 }
                 .build()
-
-class AdDataBuilder(dataBuilder: AdvertiseData.Builder) {
-    val msd = MsdBuilder(dataBuilder)
-    val serviceData = ServiceDataBuilder(dataBuilder)
-
-    class ServiceDataBuilder(private val dataBuilder: AdvertiseData.Builder) {
-        val uuids = UuidCollector(dataBuilder)
-
-        operator fun set(uuid: ParcelUuid, data: ByteArray) {
-            dataBuilder.addServiceData(uuid, data)
-        }
-    }
-
-    class MsdBuilder(private val dataBuilder: AdvertiseData.Builder) {
-        operator fun set(id: Int, data: ByteArray) {
-            dataBuilder.addManufacturerData(id, data)
-        }
-    }
-
-    class UuidCollector(private val dataBuilder: AdvertiseData.Builder) {
-        operator fun plusAssign(uuid: ParcelUuid) {
-            dataBuilder.addServiceUuid(uuid)
-        }
-    }
-}
