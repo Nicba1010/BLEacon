f9f57228cce7e3b4fcb8a542ec4989fb805ba96f
Ivan Tokić <toxxmeister@gmail.com>
2017-09-04 16:38:57 +0200

    Beaconno reorganization

--- library/src/main/java/de/troido/bleacon/beaconno/Beaconno.kt
+++ library/src/main/java/de/troido/bleacon/beaconno/Beaconno.kt
@@ -4,20 +4,6 @@ import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothGattCharacteristic
 import android.bluetooth.BluetoothProfile
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanFilter
-import android.bluetooth.le.ScanRecord
-import android.bluetooth.le.ScanResult
-import android.bluetooth.le.ScanSettings
-import android.content.Context
-import android.os.Handler
-import de.troido.bleacon.ble.HandledBleActor
-import de.troido.bleacon.ble.NORDIC_ID
-import de.troido.bleacon.ble.obtainScanner
-import de.troido.bleacon.config.scan.scanSettings
-import de.troido.bleacon.data.BleDeserializer
-import de.troido.bleacon.data.NullDeserializer
-import de.troido.bleacon.scanner.BeaconMetaData
 import de.troido.bleacon.scanner.BleChrWriter
 import de.troido.bleacon.scanner.OneTimeBleChrWriter
 import java.util.UUID
@@ -76,86 +62,5 @@ fun bleConnectionCallback(svcUuid: UUID,
                     chr.writeType = BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE
                     onBeaconnoWriter(writerCtor(chr, gatt))
                 }
-    }
-}
-
-
-class BeaconnoDevice<out T>(private val context: Context,
-                            private val deserializer: BleDeserializer<T>,
-                            private val dataTransform: (ByteArray) -> ByteArray,
-                            private val scanRecord: ScanRecord,
-                            val metaData: BeaconMetaData) {
-
-    val data: T? by lazy {
-        scanRecord.getManufacturerSpecificData(NORDIC_ID)
-                ?.let(dataTransform)
-                ?.let {
-                    when (deserializer.length) {
-                        BleDeserializer.ALL -> it
-                        in 0..it.size       -> it.copyOfRange(0, deserializer.length)
-                        else                -> null
-                    }
-                }
-                ?.let(deserializer::deserialize)
-    }
-
-    fun connect(context: Context, callback: BluetoothGattCallback) {
-        metaData.device.connectGatt(context, false, callback)
-    }
-}
-
-/**
- * Example usage:
- * ```
- * BeaconnoScanner(
- *         context,
- *         bleFilter(uuid16 = Uuid16.fromString("17CF")),
- *         Int16Deserializer,
- *         UUID16_TRANSFORM,
- *         scanSettings()
- * ) { scanner, device ->
- *     device.connect(
- *             context,
- *             bleConnectionCallback(
- *                     Uuid16.fromString("23FF").toUuid(),
- *                     Uuid16.fromString("4566").toUuid()
- *             ) { writer ->
- *                 writer.write("Hello ${device.data}!".toByteArray())
- *             }
- *     )
- *     Log.d("Beaconno", "Found a beaconno with ${device.data}!")
- * }
- * ```
- */
-class BeaconnoScanner<T>(context: Context,
-                         filter: ScanFilter,
-                         deserializer: BleDeserializer<T> = NullDeserializer(),
-                         dataTransform: (ByteArray) -> ByteArray,
-                         private val settings: ScanSettings = scanSettings(),
-                         handler: Handler = Handler(),
-                         private val listener: OnBeaconno<T>
-) : HandledBleActor(handler) {
-
-    private val scanner = obtainScanner()
-    private val filters = listOf(filter)
-
-    private val callback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            super.onScanResult(callbackType, result)
-            result.scanRecord
-                    ?.let {
-                        BeaconnoDevice(context, deserializer, dataTransform, it,
-                                       BeaconMetaData(result.device, result.rssi, it.txPowerLevel))
-                    }
-                    ?.let { listener(this@BeaconnoScanner, it) }
-        }
-    }
-
-    override fun start() {
-        handler.post { scanner.startScan(filters, settings, callback) }
-    }
-
-    override fun stop() {
-        handler.post { scanner.stopScan(callback) }
     }
 }
