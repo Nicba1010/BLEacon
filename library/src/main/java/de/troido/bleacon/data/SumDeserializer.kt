335d4af02f92b1d324ba3dfad6b4727b2e5f8d3e
Ivan Tokić <toxxmeister@gmail.com>
2017-09-05 15:31:07 +0200

    SumDeserializer prefix mask

--- library/src/main/java/de/troido/bleacon/data/SumDeserializer.kt
+++ library/src/main/java/de/troido/bleacon/data/SumDeserializer.kt
@@ -1,9 +1,11 @@
 package de.troido.bleacon.data
 
 import de.troido.ekstend.collections.copyOfRange
+import kotlin.experimental.and
 
 class SumDeserializer<out T>(
         private val removePrefix: Boolean = true,
+        private val prefixMask: Byte = 0xFF.toByte(),
         private val deserializers: Map<Byte, BleDeserializer<T>>
 ) : BleDeserializer<T> {
 
@@ -11,7 +13,7 @@ class SumDeserializer<out T>(
 
     override fun deserialize(data: ByteArray): T? = when {
         data.isEmpty() -> null
-        else           -> deserializers[data[0]]
+        else           -> deserializers[data[0] and prefixMask]
                 ?.takeIf { it.matchesLength(data) }
                 ?.deserialize(if (removePrefix) data.copyOfRange(1) else data)
     }
